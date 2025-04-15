0b9095b50776a8c50e0824af2b8752d9bdac7879
manh-lam-tpv-clv <manh.lam.tpv@one-line.com>
2025-04-15 09:21:15 +0700

    Initial commit

---
+++ build_proto.sh
@@ -0,0 +1,23 @@
+# #!/bin/bash
+
+# protoc \
+#     -I=proto \
+#     --plugin=$(pwd)/node_modules/.bin/protoc-gen-ts_proto \
+#     --ts_proto_opt=esModuleInterop=true \
+#     --ts_proto_out="./build" \
+#     proto/**/**/*.proto
+
+# -----------------------
+
+#!/bin/bash
+set -e
+
+echo "Generating TypeScript code using buf..."
+
+# Clean up previous output
+rm -rf build
+
+# Run buf generate
+buf generate
+
+echo "✅ Proto files compiled to ./build"
